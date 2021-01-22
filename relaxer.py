35f121724ef075c74f11e06f7d4364632b98d8bb
Arjun589 <58413630+Arjun589@users.noreply.github.com>
2021-01-22 20:14:32 +0530

    Update relaxer.py

--- relaxer.py
+++ relaxer.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
         notification.notify(
             title="**Please Drink Water Now!! and take some rest ",
             message="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
-            app_icon=r"C:\Users\91996\Desktop\Practise\python\Aha-Soft-Free-3d-Glossy-First-aid.ico",
+            app_icon=r"set your icon path",
             timeout=12
         )
         time.sleep(60*60)
